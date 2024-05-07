1387161c426fdeca0375ae11bb18705ffed59fae
Gregoire Parant <gregoire@workadventu.re>
2024-05-07 11:12:59 +0200

    Add opened / closed door

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,16 @@ console.log('Script started successfully');
 // Waiting for the API to be ready
 WA.onInit().then(() => {
     console.log('Scripting API ready');
-    console.log('Player tags: ',WA.player.tags)
+    console.log('Player tags: ',WA.player.tags);
+
+    WA.room.area.onEnter('conference').subscribe(() => {
+        WA.room.showLayer('floor/dooropened');
+        WA.room.hideLayer('floor/doorclosed');
+    });
+    WA.room.area.onLeave('conference').subscribe(() => {
+        WA.room.hideLayer('floor/dooropened');
+        WA.room.showLayer('floor/doorclosed');
+    });
 
     // The line below bootstraps the Scripting API Extra library that adds a number of advanced properties/features to WorkAdventure
     bootstrapExtra().then(() => {
